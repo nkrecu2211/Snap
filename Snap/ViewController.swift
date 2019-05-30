76bf1c89c16dc1bf38b29747b85a2e1a0757cd69
Nikolas <nkrecu2211@stu.d214.org>
2019-05-30 11:17:58 -0500

    snap

--- Snap/ViewController.swift
+++ Snap/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var squareView: UIView!
+    var snap: UISnapBehavior!
+    var animator: UIDynamicAnimator!
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        let tapPoint: CGPoint = sender.location(in: view)
+        
+        
+        if (snap != nil){
+            animator.removeBehavior(snap)
+        }
+        
+        snap = UISnapBehavior(item: squareView, snapTo: tapPoint)
+        animator.addBehavior(snap)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        squareView = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        squareView.backgroundColor = UIColor.blue
+        view.addSubview(squareView)
+        
+        animator = UIDynamicAnimator(referenceView: view)
+    }
+   override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
+    
+        
+        
+    }
 
-}
 
